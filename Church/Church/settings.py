b07add3e8343ca855d1f9b58dcd613ff3a6343f1
Ubuntu <ubuntu@ip-172-31-3-160.ap-northeast-1.compute.internal>
2024-12-02 03:24:27 +0000

    Save local changes

--- Church/Church/settings.py
+++ Church/Church/settings.py
@@ -33,7 +33,7 @@ DEBUG = env('DEBUG')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 # Application definition
